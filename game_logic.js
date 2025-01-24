4633e83887cc114c8bd8e536a1d154ffd82d94a6
AahanMehta21 <mehta295@purdue.edu>
2025-01-23 20:24:57 -0500

    game logic implemented

--- game_logic.js
+++ game_logic.js
@@ -1,8 +1,5 @@
 const moves = ["rock", "paper", "scissors"];
 
-let playerScore = 0;
-let computerScore = 0;
-
 function getComputerMove() {
     return moves[Math.floor(Math.random() * 3)];
 }
@@ -16,22 +13,36 @@ function getPlayerMove() {
     return null;
 }
 
-function playRound(playerMove, computerMove) {
-    if (playerMove == null) {
-        console.log("Round voided. Try again.");
-        return;
-    }
-    if (playerMove == computerMove) {
-        console.log("Round tied. Both players did the same move!");
-    } else if ((playerMove === moves[0] && computerMove === moves[2]) ||
-                (playerMove === moves[1] && computerMove === moves[0]) ||
-                (playerMove === moves[2] && computerMove === moves[1])) {
-        console.log("You win! " + playerMove + " beats " + computerMove + "!");
-        playerScore++;
-    } else {
-        console.log("You lose! " + computerMove + " beats " + playerMove + "!");
-        computerScore++;
-    }
-}
 
-playRound(getPlayerMove(), getComputerMove());
+
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let playedRounds = 0;
+    function playRound(playerMove, computerMove) {
+        if (playerMove == null) {
+            console.log("Round voided. Try again.");
+            return;
+        }
+        playedRounds++;
+        if (playerMove == computerMove) {
+            console.log("Round tied. Both players did the same move!");
+        } else if ((playerMove === moves[0] && computerMove === moves[2]) ||
+                    (playerMove === moves[1] && computerMove === moves[0]) ||
+                    (playerMove === moves[2] && computerMove === moves[1])) {
+            console.log("You win! " + playerMove + " beats " + computerMove + "!");
+            playerScore++;
+        } else {
+            console.log("You lose! " + computerMove + " beats " + playerMove + "!");
+            computerScore++;
+        }
+    }
+
+    while (playedRounds < 5) {
+        playRound(getPlayerMove(), getComputerMove());
+    }
+    console.log(`Final scores:\nYou: ${playerScore}\nComputer:${computerScore}`);
+    if (playerScore > computerScore) console.log("You win!");
+    else if (playerScore == computerScore) console.log("Its a tie!");
+    else console.log("You lose!");
+}
